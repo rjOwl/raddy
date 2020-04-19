a9281dcc5b3b3c96b1bdcf3a57548018ba5f697d
Venfox <mohamed_140@hotmail.nl>
2020-04-19 15:53:55 +0200

    Update main_bot.py

---
+++ main_bot.py
@@ -0,0 +1,15 @@
+'''
+Reddit Bot called Raddy
+This simple reddit bot will go through the feed of reddit posts and find specific keywords.
+After finding these keywords it will post a meme to add to the conversation.
+Build with the reddit API Praw : https://praw.readthedocs.io/en/latest/index.html
+'''
+import time
+import praw
+# Basic skeleton for all the different functions
+def function1():
+	pass
+def function2():
+	pass
+def function3():
+	pass
